bfa86a1e8b4fa157b72523a1fd37eaa9271581b5
zhuang biaowei <zbw@kaiyuanshe.org>
2024-11-28 21:52:54 +0800

    add get_code

--- workers/get_code.rb
+++ workers/get_code.rb
@@ -1,15 +1,19 @@
 SmartPrompt.define_worker :get_code do
-    use "siliconflow"
-    system "You are a helpful programmer."
-    model "Qwen/Qwen2.5-Coder-7B-Instruct"
-    prompt :generate_code, {
-        name: params[:name],
-        description: params[:description],
-        input: params[:input], 
-        output: params[:output]
-    }
-    code_text = send_msg
+  use "siliconflow"
+  sys_msg "You are a helpful programmer."
+  model "Qwen/Qwen2.5-Coder-7B-Instruct"
+  prompt :generate_code, {
+    name: params[:name],
+    description: params[:description],
+    input: params[:input],
+    output: params[:output]
+  }
+  code_text = safe_send_msg
+  if code_text.include?("Failed to call LLM after")
+    code_text
+  else
     model "meta-llama/Meta-Llama-3.1-8B-Instruct"
     prompt :get_code, {code_text: code_text}
-    send_msg
+    safe_send_msg
+  end
 end
