ab90c6064503ceba23955e05a50a5d3bc3584577
zhuang biaowei <zbw@kaiyuanshe.org>
2024-11-28 22:19:49 +0800

    add code.rb

--- code.rb
+++ code.rb
@@ -1,10 +1,15 @@
-require '../smart_prompt/lib/smart_prompt'
-engine = SmartPrompt::Engine.new('./config/llm_config.yml')
+require "../smart_prompt/lib/smart_prompt"
+engine = SmartPrompt::Engine.new("./config/llm_config.yml")
 result = engine.call_worker(:get_code, {
-    name: "calculate_triangle_area", 
-    description: "calculates the area of a triangle",
-    input: "the base and height of the triangle",
-    output: "the area as a float"
-    }) 
-code_str = result+"\n"+"puts calculate_triangle_area(8, 10)"
-eval(code_str)
+  name: "calculate_triangle_area",
+  description: "calculates the area of a triangle",
+  input: "the base and height of the triangle",
+  output: "the area as a float"
+})
+
+if result.include?("Failed to call LLM after")
+  puts result
+else
+  code_str = result + "\n" + "puts calculate_triangle_area(8, 10)"
+  eval(code_str)
+end
